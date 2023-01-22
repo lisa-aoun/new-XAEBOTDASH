528267fab106f17be5c0811a2966c551ba2e64b2
Lisa Aoun <lisa.aoun@xaenondigital.com>
2023-01-22 01:42:57 +0100

    initial commit

---
+++ script1pyhton.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt install python3
+python --version 
+sudo apt update
+sudo apt install virtualenv 
+mkdir Project-XAENONBOTDASH
+cd Project-XAENONBOTDASH
+sudo apt intall python3.10-venv 
+source Project-XAENONBOTDASH/bin/activate
+
--- shell_script/script1pyhton.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-virtualenv -q -p /usr/bin/python3.5 $1
-source $1/bin/activate
-pip install -r requirements.txt
-
